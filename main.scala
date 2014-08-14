e554514c33e01760a2cd4170be44b5fa1a98c499
gogotanaka <qlli.illb@gmail.com>
2014-08-14 10:33:34 -0700

    Q2

--- main.scala
+++ main.scala
@@ -1,6 +1,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println(q1)
+    println("A1 is -> " + q1)
+    println("A2 is -> " + q2)
   }
 
   def q1 = {
@@ -12,13 +13,19 @@ object Main {
     (1 until 1000).view.filter(n => n % 3 == 0 || n % 5 == 0).sum
   }
 
+  def q2 = {
+    // def fib(n: Int): Int = n match {
+    //   case 1 | 2 => 1
+    //   case _     => fib(n - 1) + fib(n - 2)
+    // }
+    lazy val fs: Stream[Int] =
+      0 #:: 1 #:: fs.zip(fs.tail).map(p => p._1 + p._2)
+
+    fs.view.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+  }
+
   def fact(n: Int): Int = n match {
     case 1 => 1
     case _ => n * fact(n-1)
-  }
-
-  def fib(n: Int): Int = n match {
-    case 1 | 2 => 1
-    case _     => fib(n - 1) + fib(n - 2)
   }
 }
